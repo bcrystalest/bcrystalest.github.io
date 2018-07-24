7444b0de56ba1b18b21793dd09d2cebed85732ae
Louis.Chen <834112686@163.com>
2018-07-24 13:35:55 +0800

    update

---
+++ foreo/test1/product_files/checkdevice.js
@@ -0,0 +1,25 @@
+var deviceChecker={};
+
+deviceChecker.isApple=()=>{
+    return /foreoapp/i.test(navigator.userAgent) && /(iPhone|iPad|iOS)/i.test(navigator.userAgent);
+    //alert(navigator.userAgent);
+}
+
+deviceChecker.isAndroid=()=>{
+    return /foreoapp/i.test(navigator.userAgent) && /(Android)/i.test(navigator.userAgent);
+}
+deviceChecker.Toast=(msg,duration)=>{  
+    duration=isNaN(duration)?3000:duration;  
+    var m = document.createElement('div');  
+    m.innerHTML = msg;  
+    m.style.cssText="width:60%; min-width:150px; background:#000; opacity:0.5; height:40px; color:#fff; line-height:40px; text-align:center; border-radius:5px; position:fixed; top:40%; left:20%; z-index:999999; font-weight:bold;";  
+    document.body.appendChild(m);  
+    setTimeout(function() {  
+        var d = 0.5;  
+        m.style.webkitTransition = '-webkit-transform ' + d + 's ease-in, opacity ' + d + 's ease-in';  
+        m.style.opacity = '0';  
+        setTimeout(function() { document.body.removeChild(m) }, d * 1000);  
+    }, duration);  
+}
+
+
