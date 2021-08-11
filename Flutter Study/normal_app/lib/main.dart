3d9b47baf08fcd20cde37d7a1609735b05780bdd
Louis.Chen <834112686@163.com>
2021-08-11 10:15:08 +0800

    no message

--- Flutter Study/normal_app/lib/main.dart
+++ Flutter Study/normal_app/lib/main.dart
@@ -127,28 +127,25 @@ class WebViewExampleState extends State<WebViewExample> {
         // _webViewController.evaluateJavascript("flutterCallJsMethod('Flutter通过JS修改')");
       },
       javascriptMode: JavascriptMode.unrestricted,
-      initialUrl: 'https://akamai.wwwqa.brandap.ford.com/content/cx727/cn/zh_cn/user.html?t=23323332312',
-      // initialUrl: 'https://dragonsoftbravo.oss-cn-shanghai.aliyuncs.com/727app/ecommerce.html?t=991',
+      initialUrl: 'https://akamai.wwwqa.brandap.ford.com/content/cx727/cn/zh_cn/home/mach-e.html?t=292',
+      // initialUrl: 'https://www.baidu.com',
       // initialUrl: 'https://cofdev.csmc-cloud.com/marchevapp/727evapp.html?t=9119111101',
-      // initialUrl: 'https://akamai.wwwqa.brandap.ford.com/content/cx727/cn/zh_cn/user.html',
-      // initialUrl: 'https://wwwdev.fafc.cnps.dealerconnection.com.cn/cashier/v1/demoPc/home.html',
       // initialUrl: 'https://dsl.apps.pp01.cneast.cf.ford.com.cn/content/cx727/cn/zh_cn/model-select.html#model?t=91222211',
       // initialUrl: 'https://bcrystalest.github.io/flutterNative.html?t=222223111',
-      // initialUrl: 'https://baoming-dev.csmc-cloud.com/armstrade/#/courseVideoWare?courseId=105264463607&url=https%3A%2F%2Fappdata-qas.s3.cn-north-1.amazonaws.com.cn%2FPSS%2FIMAGE%2FTRAIN%2F202104%2FFX6%25E7%25AE%2580%25E6%2598%2593%25E8%25AE%25BE%25E5%25AE%259A%25E6%258C%2587%25E5%258D%2597-%25E5%25BF%25AB%25E9%2580%259F%25E5%25AE%2589%25E8%25A3%2585.mp4%3FX-Amz-Algorithm%3DAWS4-HMAC-SHA256%26X-Amz-Date%3D20210426T065700Z%26X-Amz-SignedHeaders%3Dhost%26X-Amz-Expires%3D7198%26X-Amz-Credential%3DAKIATKVQAB6F2YBT2CJZ%252F20210426%252Fcn-north-1%252Fs3%252Faws4_request%26X-Amz-Signature%3D482db24cef0a4246f618103605e89035a04b40d3e87908cfaa4bfe02ea3ffc6b',
       gestureNavigationEnabled: true,
       userAgent: "EVWebviewChannel",
       javascriptChannels: {
         JavascriptChannel(
           name:"sendPayMessage",
           onMessageReceived: (JavascriptMessage message){
-            print("获取JS回传数据: ${message.message}");
+            print("sendPayMessage获取JS回传数据: ${message.message}");
             // _showDialog(context);
           }
         ),
         JavascriptChannel(
             name:"EVWebviewChannel",
             onMessageReceived: (JavascriptMessage message){
-              print("获取JS回传数据: ${message.message}");
+              print("EVWebviewChannel获取JS回传数据: ${message.message}");
               // _showDialog(context);
             }
         )
